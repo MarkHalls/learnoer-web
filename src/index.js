2ca388fe6b69b4e3ae9af9be4abd3c215c5795b6
Mark Halls <mark.halls@gmail.com>
2020-05-12 05:13:20 -0700

    Rename BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import "normalize.css";
 import "./index.less";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
