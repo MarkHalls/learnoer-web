020f5ee08d4d67e0c10b42abdeea7ed1132eb6f8
Mark Halls <mark.halls@gmail.com>
2020-05-15 13:50:55 -0700

    Add outside link icon

---
+++ src/components/atoms/OutsideLinkIcon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const OutsideLinkIcon = () => {
+  return (
+    <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" className="outsideLink">
+      <path
+        d="M3.167 4h4v1.333H4.5V12h6.667V9.333H12.5v4H3.167V4z"
+        fill="#0099D0"
+      />
+      <path
+        d="M8.5 2.667h5.333V8l-2-2-2.666 2.667-1.334-1.334L10.5 4.667l-2-2z"
+        fill="#0099D0"
+      />
+    </svg>
+  );
+};
+
+export default OutsideLinkIcon;
