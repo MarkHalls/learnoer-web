588cd37edacbbdc4f5fc807cf48564cc556cd7be
Mark Halls <mark.halls@gmail.com>
2020-05-15 13:51:16 -0700

    Fix styling on BookCard

--- src/components/molecules/BookCard.jsx
+++ src/components/molecules/BookCard.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import DefaultCover from "../atoms/DefaultCover";
+import OutSideLinkIcon from "../atoms/OutsideLinkIcon";
+
+import "./BookCard.less";
 
 const BookCard = ({
   title,
@@ -13,30 +16,31 @@ const BookCard = ({
   bookKey,
 }) => {
   return (
-    <div className="card">
-      <div className="card-book">
-        {cover ? (
-          <img
-            src={cover}
-            alt={`${title} cover image`}
-            className="book-cover"
-          />
-        ) : (
-          <DefaultCover className="book-cover" />
-        )}
-        <h2 className="book-title">{title}</h2>
+    <div className="book">
+      <div className="book-card">
+        <Link to={bookKey} className="book-card-cover">
+          {cover ? (
+            <img src={cover} alt={`${title} cover image`} />
+          ) : (
+            <DefaultCover />
+          )}
+        </Link>
+        <Link to={bookKey} className="book-card-title">
+          {title}
+        </Link>
         <p>
-          by <span className="book-author">{author}</span>
+          by <span className="book-card-author">{author}</span>
         </p>
-        <p>published by {publisher}</p>
-        <Link to={`${bookKey}`} className="book-reviewCount">
+        <p>{`published by ${publisher}`}</p>
+        <Link to={`${bookKey}`} className="book-card-reviewCount">
           {reviewCount} reviews
         </Link>
       </div>
-      <a href={url} className="card-read button">
-        Read now
+      <a href={url} className="book-read button">
+        <span>Read now </span>
+        <OutSideLinkIcon />
       </a>
-      <Link to={`${bookKey}/#compose`} className="card-review button">
+      <Link to={`${bookKey}/#compose`} className="book-review button">
         Write a review
       </Link>
     </div>
