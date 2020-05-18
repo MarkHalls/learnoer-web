48d69ded6477874ade098fa33215904e8d7e1aed
Mark Halls <mark.halls@gmail.com>
2020-05-18 10:39:35 -0700

    Reset search term when visiting home page

--- src/components/organisms/HomePage.jsx
+++ src/components/organisms/HomePage.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
+import actions from "../../actions";
 import SearchBar from "../molecules/SearchBar";
 import SiteLogo from "../atoms/SiteLogo";
 
 import "./HomePage.less";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(actions.setSearchTerm("")), []);
+
   return (
     <div className="content">
       <SiteLogo className="content-logo" />
       <h1 className="content-title">Learn OER</h1>
       <SearchBar className="content-search" />
-      <p className="content-description">Search by title or isbn and we'll find the closest matching book that can be read online for free.</p>
+      <p className="content-description">
+        Search by title or isbn and we'll find the closest matching book that
+        can be read online for free.
+      </p>
     </div>
   );
 };
